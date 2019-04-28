1298e7a7d8ee812e9072212542d1f2584ffb2dba
reddit-reaper <35949207+redditreaper86@users.noreply.github.com>
2019-04-28 16:42:15 -0400

    scraper updates

--- lib/openscrapers/sources_openscrapers/en_DebridOnly/mvrls.py
+++ lib/openscrapers/sources_openscrapers/en_DebridOnly/mvrls.py
@@ -1,16 +1,6 @@
 # -*- coding: utf-8 -*-
 
-#  ..#######.########.#######.##....#..######..######.########....###...########.#######.########..######.
-#  .##.....#.##.....#.##......###...#.##....#.##....#.##.....#...##.##..##.....#.##......##.....#.##....##
-#  .##.....#.##.....#.##......####..#.##......##......##.....#..##...##.##.....#.##......##.....#.##......
-#  .##.....#.########.######..##.##.#..######.##......########.##.....#.########.######..########..######.
-#  .##.....#.##.......##......##..###.......#.##......##...##..########.##.......##......##...##........##
-#  .##.....#.##.......##......##...##.##....#.##....#.##....##.##.....#.##.......##......##....##.##....##
-#  ..#######.##.......#######.##....#..######..######.##.....#.##.....#.##.......#######.##.....#..######.
-
 '''
-    OpenScrapers Project
-
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
     the Free Software Foundation, either version 3 of the License, or
@@ -25,33 +15,27 @@
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 '''
 
-
-
 import re
-import traceback
 import urllib
 import urlparse
 
-from openscrapers.modules import cleantitle, client, debrid, log_utils, source_utils, cfscrape
+from openscrapers.modules import cleantitle, client, debrid, source_utils
 
 
 class source:
     def __init__(self):
         self.priority = 1
         self.language = ['en']
-        self.domains = ['0dayreleases.com', '0dayreleases.biz']
-        self.base_link = 'https://0dayreleases.com/'
+        self.domains = ['mvrls.com']
+        self.base_link = 'http://mvrls.com'
         self.search_link = '/search/%s/feed/rss2/'
-        self.scraper = cfscrape.create_scraper()
 
     def movie(self, imdb, title, localtitle, aliases, year):
         try:
             url = {'imdb': imdb, 'title': title, 'year': year}
             url = urllib.urlencode(url)
             return url
-        except Exception:
-            failure = traceback.format_exc()
-            log_utils.log('0DAY - Exception: \n' + str(failure))
+        except:
             return
 
     def tvshow(self, imdb, tvdb, tvshowtitle, localtvshowtitle, aliases, year):
@@ -59,9 +43,7 @@ class source:
             url = {'imdb': imdb, 'tvdb': tvdb, 'tvshowtitle': tvshowtitle, 'year': year}
             url = urllib.urlencode(url)
             return url
-        except Exception:
-            failure = traceback.format_exc()
-            log_utils.log('0DAY - Exception: \n' + str(failure))
+        except:
             return
 
     def episode(self, url, imdb, tvdb, title, premiered, season, episode):
@@ -74,17 +56,15 @@ class source:
             url['title'], url['premiered'], url['season'], url['episode'] = title, premiered, season, episode
             url = urllib.urlencode(url)
             return url
-        except Exception:
-            failure = traceback.format_exc()
-            log_utils.log('0DAY - Exception: \n' + str(failure))
+        except:
             return
 
     def sources(self, url, hostDict, hostprDict):
         try:
             sources = []
 
-            if url is None:
-                return sources
+            if url is None: return sources
+            if debrid.status() == False: raise Exception()
 
             data = urlparse.parse_qs(url)
             data = dict([(i, data[i][0]) if data[i] else (i, '') for i in data])
@@ -93,18 +73,14 @@ class source:
 
             hdlr = 'S%02dE%02d' % (int(data['season']), int(data['episode'])) if 'tvshowtitle' in data else data['year']
 
-            query = '%s S%02dE%02d' % (
-                data['tvshowtitle'],
-                int(data['season']),
-                int(data['episode'])) if 'tvshowtitle' in data else '%s %s' % (
-                data['title'],
-                data['year'])
+            query = '%s S%02dE%02d' % (data['tvshowtitle'], int(data['season']), int(data['episode'])) if 'tvshowtitle' in data else '%s %s' % (
+                data['title'], data['year'])
             query = re.sub('(\\\|/| -|:|;|\*|\?|"|\'|<|>|\|)', ' ', query)
 
             url = self.search_link % urllib.quote_plus(query)
             url = urlparse.urljoin(self.base_link, url)
 
-            html = self.scraper.get(url).content
+            html = client.request(url)
             posts = client.parseDOM(html, 'item')
 
             hostDict = hostprDict + hostDict
@@ -114,25 +90,24 @@ class source:
             for post in posts:
                 try:
                     t = client.parseDOM(post, 'title')[0]
-                    u = client.parseDOM(post, 'a', ret='href')
+                    u = client.parseDOM(post, 'enclosure', ret='url')
                     s = re.search('((?:\d+\.\d+|\d+\,\d+|\d+)\s*(?:GB|GiB|MB|MiB))', post)
                     s = s.groups()[0] if s else '0'
                     items += [(t, i, s) for i in u]
-                except Exception:
+                except:
                     pass
 
             for item in items:
                 try:
 
                     url = item[1]
-                    if any(x in url for x in ['.rar', '.zip', '.iso']):
-                        raise Exception()
+                    if any(x in url for x in ['.rar', '.zip', '.iso', '.part']): raise Exception()
                     url = client.replaceHTMLCodes(url)
                     url = url.encode('utf-8')
 
                     valid, host = source_utils.is_host_valid(url, hostDict)
-                    if not valid:
-                        raise Exception()
+                    if not valid: raise Exception()
+
                     host = client.replaceHTMLCodes(host)
                     host = host.encode('utf-8')
 
@@ -146,8 +121,7 @@ class source:
 
                     y = re.findall('[\.|\(|\[|\s](\d{4}|S\d*E\d*|S\d*)[\.|\)|\]|\s]', name)[-1].upper()
 
-                    if not y == hdlr:
-                        raise Exception()
+                    if not y == hdlr: raise Exception()
 
                     quality, info = source_utils.get_release_quality(name, url)
 
@@ -157,14 +131,14 @@ class source:
                         size = float(re.sub('[^0-9|/.|/,]', '', size)) / div
                         size = '%.2f GB' % size
                         info.append(size)
-                    except Exception:
+                    except:
                         pass
 
                     info = ' | '.join(info)
 
                     sources.append({'source': host, 'quality': quality, 'language': 'en', 'url': url, 'info': info,
-                                    'direct': False, 'debridonly': debrid.status()})
-                except Exception:
+                                    'direct': False, 'debridonly': True})
+                except:
                     pass
 
             check = [i for i in sources if not i['quality'] == 'CAM']
@@ -172,9 +146,7 @@ class source:
                 sources = check
 
             return sources
-        except Exception:
-            failure = traceback.format_exc()
-            log_utils.log('0DAY - Exception: \n' + str(failure))
+        except:
             return sources
 
     def resolve(self, url):
