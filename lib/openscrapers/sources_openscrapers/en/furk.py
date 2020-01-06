1766bc399619c4fc585d75dfa4753561dcf706ed
Tikipeter <28930002+Tikipeter@users.noreply.github.com>
2020-01-07 07:52:40 +1100

    Update furk.py

--- lib/openscrapers/sources_openscrapers/en/furk.py
+++ lib/openscrapers/sources_openscrapers/en/furk.py
@@ -9,19 +9,19 @@
 #  ..#######.##.......#######.##....#..######..######.##.....#.##.....#.##.......#######.##.....#..######.
 
 '''
-    OpenScrapers Project
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
+	OpenScrapers Project
+	This program is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
 
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
 
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+	You should have received a copy of the GNU General Public License
+	along with this program.  If not, see <http://www.gnu.org/licenses/>.
 '''
 
 import json
@@ -30,8 +30,8 @@ import sys
 import requests
 
 from openscrapers.modules import control
+from openscrapers.modules import cleantitle
 from openscrapers.modules import source_utils
-
 
 class source:
 	def __init__(self):
@@ -39,37 +39,34 @@ class source:
 		self.language = ['en']
 		self.domain = 'furk.net/'
 		self.base_link = 'https://www.furk.net'
-		self.meta_search_link = "/api/plugins/metasearch?api_key=%s&q=%s&cached=yes" \
-		                        "&match=%s&moderated=%s%s&sort=relevance&type=video&offset=0&limit=%s"
+		self.search_link = "/api/plugins/metasearch?api_key=%s&q=%s&cached=yes" \
+								"&match=%s&moderated=%s%s&sort=relevance&type=video&offset=0&limit=200"
 		self.tfile_link = "/api/file/get?api_key=%s&t_files=1&id=%s"
 		self.login_link = "/api/login/login?login=%s&pwd=%s"
-		self.user_name = control.setting('furk.user_name')
-		self.user_pass = control.setting('furk.user_pass')
-		self.api_key = control.setting('furk.api')
-		self.search_limit = control.setting('furk.limit')
 		self.files = []
 
 	def get_api(self):
 
 		try:
 
-			api_key = self.api_key
+			user_name = control.setting('furk.user_name')
+			user_pass = control.setting('furk.user_pass')
+			api_key = control.setting('furk.api')
 
 			if api_key == '':
-				if self.user_name == '' or self.user_pass == '':
+				if user_name == '' or user_pass == '':
 					return
 
-				else:
-					s = requests.Session()
-					link = (self.base_link + self.login_link % (self.user_name, self.user_pass))
-					p = s.post(link)
-					p = json.loads(p.text)
+				s = requests.Session()
+				link = (self.base_link + self.login_link % (user_name, user_pass))
+				p = s.post(link)
+				p = json.loads(p.text)
 
-					if p['status'] == 'ok':
-						api_key = p['api_key']
-						control.setSetting('furk.api', api_key)
-					else:
-						pass
+				if p['status'] == 'ok':
+					api_key = p['api_key']
+					control.setSetting('furk.api', api_key)
+				else:
+					pass
 
 			return api_key
 
@@ -115,22 +112,23 @@ class source:
 			match = 'extended'
 			moderated = 'no' if content_type == 'episode' else 'yes'
 			search_in = ''
-
+		
 			if content_type == 'movie':
-				title = url['title'].replace(':', ' ').replace(' ', '+').replace('&', 'and')
-				title = title.replace("'", "")
-				year = url['year']
-				link = '@name+%s+%s+@files+%s+%s' % (title, year, title, year)
+				title = cleantitle.normalize(url.get('title'))
+				year = url.get('year')
+				query = '@name+%s+%s+@files+%s+%s' % (title, year, title, year)
 
 			elif content_type == 'episode':
-				title = url['tvshowtitle'].replace(':', ' ').replace(' ', '+').replace('&', 'and')
+				title = cleantitle.normalize(url.get('tvshowtitle'))
 				season = int(url['season'])
 				episode = int(url['episode'])
-				link = self.makeQuery(title, season, episode)
+				seasEpList = self._seas_ep_query_list(season, episode)
+				query = '@name+%s+@files+%s+|+%s+|+%s+|+%s+|+%s' % (title, seasEpList[0], seasEpList[1], seasEpList[2], seasEpList[3], seasEpList[4])
+			
 
 			s = requests.Session()
-			link = self.base_link + self.meta_search_link % \
-			       (api_key, link, match, moderated, search_in, self.search_limit)
+			link = self.base_link + self.search_link % \
+				   (api_key, query, match, moderated, search_in)
 
 			p = s.get(link)
 			p = json.loads(p.text)
@@ -141,30 +139,35 @@ class source:
 			files = p['files']
 
 			for i in files:
+				
 				if i['is_ready'] == '1' and i['type'] == 'video':
+					
 					try:
+
 						source = 'SINGLE'
 						if int(i['files_num_video']) > 3:
 							source = 'PACK [B](x%02d)[/B]' % int(i['files_num_video'])
 						file_name = i['name']
 						file_id = i['id']
 						file_dl = i['url_dl']
+						size = float(i['size']) / 1073741824
+						
 						if content_type == 'episode':
-							url = '%s<>%s<>%s' % (file_id, season, episode)
-							details = self.details(file_name, i['size'], i['video_info'])
+							url = json.dumps({'content': 'episode', 'file_id': file_id, 'season': season, 'episode': episode})
 						else:
-							url = '%s<>%s<>%s+%s' % (file_id, 'movie', title, year)
-							details = self.details(file_name, i['size'], i['video_info']).split('|')
-							details = details[0] + ' | ' + file_name.replace('.', ' ')
+							url = json.dumps({'content': 'movie', 'file_id': file_id, 'title': title, 'year': year})
 
-						quality = source_utils.get_release_quality(file_name, file_dl)
+						quality = source_utils.get_release_quality(file_name, file_dl)[0]
+						info = source_utils.getFileType(file_name)
+						info = '%.2f GB | %s | %s' % (size, info, file_name.replace('.', ' ').upper())
 						sources.append({'source': source,
-						                'quality': quality[0],
-						                'language': "en",
-						                'url': url,
-						                'info': details,
-						                'direct': True,
-						                'debridonly': False})
+										'quality': quality,
+										'language': "en",
+										'url': url,
+										'info': info,
+										'direct': True,
+										'debridonly': False})
+
 					except:
 						pass
 
@@ -182,15 +185,21 @@ class source:
 	def resolve(self, url):
 
 		try:
+			
+			api_key = self.get_api()
 
-			info = url.split('<>')
-			file_id = info[0]
+			if not api_key:
+				return
 
-			self.content_type = 'movie' if info[1] == 'movie' else 'episode'
+			url = json.loads(url)
 
-			if self.content_type == 'episode': self.filtering_list = self.seasEpQueryList(info[1], info[2])
+			file_id = url.get('file_id')
 
-			link = (self.base_link + self.tfile_link % (self.api_key, file_id))
+			self.content_type = 'movie' if url.get('content') == 'movie' else 'episode'
+
+			if self.content_type == 'episode': self.filtering_list = self._seas_ep_resolve_list(url.get('season'), url.get('episode'))
+
+			link = (self.base_link + self.tfile_link % (api_key, file_id))
 			s = requests.Session()
 			p = s.get(link)
 			p = json.loads(p.text)
@@ -207,7 +216,7 @@ class source:
 				else:
 					self.files.append(i)
 
-			url = self.managePack()
+			url = self._manage_pack()
 
 			return url
 
@@ -217,71 +226,37 @@ class source:
 			print(exc_type, exc_tb.tb_lineno)
 			pass
 
-	def managePack(self):
+	def _manage_pack(self):
 
 		for i in self.files:
-			name = i['name']
+
 			if self.content_type == 'movie':
 				if 'is_largest' in i:
 					url = i['url_dl']
+
 			else:
+				name = cleantitle.get_simple(i['name'])
 				if 'furk320' not in name.lower() and 'sample' not in name.lower():
 					for x in self.filtering_list:
 						if x in name.lower():
 							url = i['url_dl']
 						else:
 							pass
+		
 		return url
 
-	def details(self, name, size, video_info):
-
-		import HTMLParser, re
-
-		name = re.sub("(&#[0-9]+)([^;^0-9]+)", "\\1;\\2", name)
-		name = HTMLParser.HTMLParser().unescape(name)
-		name = name.replace("&quot;", "\"")
-		name = name.replace("&amp;", "&")
-		size = float(size) / 1073741824
-		fmt = re.sub('(.+)(\.|\(|\[|\s)(\d{4}|S\d*E\d*)(\.|\)|\]|\s)', '', name)
-		fmt = re.split('\.|\(|\)|\[|\]|\s|\-', fmt)
-		fmt = [x.lower() for x in fmt]
-		if '3d' in fmt:
-			q = '  | 3D'
-		else:
-			q = ''
-		try:
-			info = video_info.replace('\n', '')
-			v = re.compile('Video: (.+?),').findall(info)[0]
-			a = re.compile('Audio: (.+?), .+?, (.+?),').findall(info)[0]
-			info = '%.2f GB%s | %s | %s | %s' % (size, q, v, a[0], a[1])
-			info = re.sub('\(.+?\)', '', info)
-			info = info.replace('stereo', '2.0')
-			info = info.replace('eac3', 'dd+')
-			info = info.replace('ac3', 'dd')
-			info = info.replace('channels', 'ch')
-			info = ' '.join(info.split())
-			return info
-		except:
-			pass
-		try:
-			if any(i in ['hevc', 'h265', 'x265'] for i in fmt):
-				v = 'HEVC'
-			else:
-				v = 'h264'
-			info = '%.2f GB%s | %s' % (size, q, v)
-			return info
-		except:
-			pass
-		try:
-			info = '%.2f GB | [I]%s[/I]' % (size, name.replace('.', ' '))
-			return info
-		except:
-			pass
-
-	def makeQuery(self, title, season, episode):
-		seasEpList = self.seasEpQueryList(season, episode)
-		return '@name+%s+@files+%s+|+%s+|+%s' % (title, seasEpList[0], seasEpList[1], seasEpList[2])
-
-	def seasEpQueryList(self, season, episode):
-		return ['s%02de%02d' % (int(season), int(episode)), '%dx%02d' % (int(season), int(episode)),
-		        '%02dx%02d' % (int(season), int(episode))]
+	def _seas_ep_query_list(self, season, episode):
+		
+		return ['s%02de%02d' % (season, episode),
+				'%dx%02d' % (season, episode),
+				'%02dx%02d' % (season, episode),
+				'"season %d episode %d"' % (season, episode),
+				'"season %02d episode %02d"' % (season, episode)]
+	
+	def _seas_ep_resolve_list(self, season, episode):
+		
+		return ['s%02de%02d' % (season, episode),
+				'%dx%02d' % (season, episode),
+				'%02dx%02d' % (season, episode),
+				'season%depisode%d' % (season, episode),
+				'season%02depisode%02d' % (season, episode)]
